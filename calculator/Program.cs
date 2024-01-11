ef920dab5670ca34be2c8d71ee6a84ee4f889cf0
Dalia Lozano <dalialozano@gmail.com>
2024-01-11 08:28:16 +0100

    Cleanup

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -40,21 +40,7 @@ namespace calculator
 
         static void Main(string[] args)
         {
-            // Välkomnande meddelande
-            // En lista för att spara historik för räkningar
-            // Användaren matar in tal och matematiska operation
-            // OBS! Användaren måsta mata in ett tal för att kunna ta sig vidare i programmet!
-            // Ifall användaren skulle dela med 0 visa Ogiltig inmatning!
-            // Lägga resultat till listan
-            // Visa resultat
-            // Fråga användaren om den vill visa tidigare resultat.
-            // Visa tidigare resultat
-            // Fråga användaren om den vill avsluta eller fortsätta
-
-
-            string input2 = "";
             int input3;
-            int answer = 0;
             List<int> history = new List<int>();
 
             Console.WriteLine("Welcome to the calculator");
@@ -66,11 +52,23 @@ namespace calculator
             // The only other variable we need is for storing the answers.
             // We input the first number into the variable 'answer' and use that for the next calculations.
 
-            answer = InputInt();
+            int answer = InputInt();
 
             while (true)
             {
-                input2 = InputOperator();
+                // Välkomnande meddelande
+                // En lista för att spara historik för räkningar
+                // Användaren matar in tal och matematiska operation
+                // OBS! Användaren måsta mata in ett tal för att kunna ta sig vidare i programmet!
+                // Ifall användaren skulle dela med 0 visa Ogiltig inmatning!
+                // Lägga resultat till listan
+                // Visa resultat
+                // Fråga användaren om den vill visa tidigare resultat.
+                // Visa tidigare resultat
+                // Fråga användaren om den vill avsluta eller fortsätta
+
+
+                string input2 = InputOperator();
                 // I assume we only make a choice of 'h' or 'x' when we are at the beginning of our calculation, not in the middle of it.
                 if (input2.ToLower() == "h")
                 {
